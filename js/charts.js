9ddbb8aa144d221a4f5c37e9eb9ce2510bdd333f
anipingping <106618404+anipingping@users.noreply.github.com>
2022-09-08 12:29:12 -0700

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -58,25 +58,30 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-var samplesArray = [metadata]
+  var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-var filteredSamples = metadata.filter(sampleObj => sampleObj.id == sample);
+  var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-var firstSample = 
+  var result = resultArray[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 
+    var yticks = otu_ids.slice(0, 10).map(otuID => OTU ${otuID}).reverse();
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var trace = {
+      y: yticks,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar"
+    };
+    var data = [trace];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "OTU Samples",
